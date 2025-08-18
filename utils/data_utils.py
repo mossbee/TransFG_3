a7b1253f3a6655277d2d43f65617d97773044ff4
mossbee <hoangnhatdb@gmail.com>
2025-08-18 17:38:58 +0700

    update

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -33,7 +33,7 @@ def get_loader(args):
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         test_transform=transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-        trainset = ND_TWIN(root=args.data_root, is_train=True, transform=train_transform)
+        trainset = ND_TWIN(root=args.data_root, transform=train_transform)
         verification_pairs_file = os.path.join(args.data_root, 'test_pairs.txt')
         testset = ND_TWIN_Verify(root=args.data_root, pairs_file=verification_pairs_file, transform=test_transform)
 
